ec6f14ea7f72a222899393a086384c076e3b9f47
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2023-12-04 12:16:02 +0100

    Cleaned up the code in Day_2_Cube_Conundrum and added comments

--- Day_2_Cube_Conundrum/main.py
+++ Day_2_Cube_Conundrum/main.py
@@ -1,16 +1,16 @@
 from pydantic import BaseModel
 
 
-class Game(BaseModel):
+class Game(BaseModel): # Simply class for data
     id: int = 0
     red: int = 0
     green: int = 0
     blue: int = 0
 
 
-with open('Game.txt', 'r') as f:
+with open('Game.txt', 'r') as f: #Here you can change the name of the file
     lines = f.readlines()
-
+# Here you can change the max values of cubes
 max_red = 12
 max_green = 13
 max_blue = 14
@@ -19,14 +19,11 @@ counter = 0
 for i in lines:
     separate.append(i.split(";"))
 list_of_obj = []
-test = ['Game 1: 1 red, 10 blue, 5 green', ' 11 blue, 6 green', ' 6 green', ' 1 green, 1 red, 12 blue', ' 3 blue',
-        ' 3 blue, 4 green, 1 red\n']
-
+# in this loop we create objects for each game and find max values of cubes
 for i in separate:
     counter += 1
     list_of_obj.append(Game(id=counter))
     for j in i:
-        # j = j.replace(" ", "")
         if j.__contains__("red"):
             temp_red = j[j.find("red") - 3: j.find("red")-1]
             if temp_red[0].isdigit() and temp_red[1].isdigit():
@@ -53,14 +50,16 @@ for i in separate:
                 list_of_obj[counter - 1].blue = int(temp_blue[1])
 
 sum_id = 0
+for i in list_of_obj:
+    if(i.green <= max_green and i.red <= max_red and i.blue <= max_blue): # Here we check if the game is playable
+        print(f'id: {i.id} red: {i.red} green: {i.green} blue: {i.blue}')
+        sum_id += i.id
+print(f'First answer: {sum_id}') # Answer: 2563
+
+
 sum_min_cubes = 0
-# for i in list_of_obj:
-#     if(i.green <= max_green and i.red <= max_red and i.blue <= max_blue):
-#         print(f'id: {i.id} red: {i.red} green: {i.green} blue: {i.blue}')
-#         sum_id += i.id
-# print(sum_id)
 for i in list_of_obj:
     all_cubes_in_one_game = i.red * i.green * i.blue
     sum_min_cubes += all_cubes_in_one_game
-print(sum_min_cubes)
+print(f'Second answer: {sum_min_cubes}')  # Answer: 70768
 
